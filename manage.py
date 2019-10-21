6f5c35d36ce9bb651e5721aee065bde41b11b909
Timothy Oliver <oliverwakogi@live.com>
2019-10-21 08:09:44 +0300

    create interactive shell for database

--- manage.py
+++ manage.py
@@ -1,19 +1,20 @@
 from app import create_app, db
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
+from app.models import User
 
-#creating app instance
+# creating app instance
 app = create_app('development')
 
-#command to serve app
+# command to serve app
 manager = Manager(app)
 manager.add_command('server', Server)
 
-#command to handle database migration
+# command to handle database migration
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
-#command to run unit tests. change app configuration to test
+# command to run unit tests. change app configuration to test
 @manager.command
 def test():
   '''
@@ -23,6 +24,11 @@ def test():
   import unittest
   tests = unittest.TestLoader().discover('tests')
   unittest.TextTestRunner(verbosity=2).run(tests)
+
+# command to create shell to interact with database
+@manager.shell
+def make_shell_context():
+  return dict(app=app, db=db, User=User)
 
 if __name__ == "__main__":
     manager.run()
