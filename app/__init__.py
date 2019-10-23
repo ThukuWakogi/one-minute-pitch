a24f0b4bc1ee73c32bbd9886bdaa0934e798234c
Timothy Oliver <oliverwakogi@live.com>
2019-10-23 12:17:15 +0300

    add login manager extension and instantialize auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,11 @@ from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from sassutils.wsgi import SassMiddleware
+from flask_login import LoginManager
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 db = SQLAlchemy()
 
 def create_app(config_name):
@@ -14,12 +18,14 @@ def create_app(config_name):
   # register blueprints
   from .main import main as main_blueprint
   app.register_blueprint(main_blueprint)
-  # TODO: auth blueprint registration
+  from .auth import auth as auth_blueprint
+  app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
 
   # initialize app extensions
   app.wsgi_app = SassMiddleware(app.wsgi_app, {
     'app': ('static/sass', 'static/css', '/static/css')
   })
   db.init_app(app)
+  login_manager.init_app(app)
 
   return app
